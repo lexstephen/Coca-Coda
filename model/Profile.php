007daf15ae0be73c6ddf41eb5ea530bb37bc5a16
Lex Stephen <imaginaryfilm@gmail.com>
2015-09-03 11:38:54 -0400

    Improved user login

--- model/Profile.php
+++ model/Profile.php
@@ -8,14 +8,13 @@ class Profile {
         $username = isset($_POST['username'])?$_POST['username']:null;
         // client-side we made sure both passwords matched, here we just grab the first
         $password = isset($_POST['password1'])?$_POST['password1']:null; 
-        // salt it & hash it
-        $token = hash('ripemd128',"$salt1$password$salt2");
         $first_name = isset($_POST['first_name'])?$_POST['first_name']:null;
         $last_name = isset($_POST['last_name'])?$_POST['last_name']:null;
         $email_address = isset($_POST['email_address'])?$_POST['email_address']:null;
         $image = isset($_POST['image'])?$_POST['image']:null;
         $website = isset($_POST['website'])&&($_POST['website']!='http://')?$_POST['website']:null;
         
+        // salt it & hash it
         require 'salt.php';
         
         // fire up the database connection
@@ -43,7 +42,6 @@ class Profile {
         $password_tmp = isset($_POST['password1'])?$_POST['password1']:null; 
         // salt it & hash it
         require 'salt.php';
-        $token = hash('ripemd128',"$salt1$password_tmp$salt2");
        
         // fire up the database connection
         $db = Db::getInstance();
